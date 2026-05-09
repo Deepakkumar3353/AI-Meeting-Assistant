723e7a3b8611beee3b039b4fc072cc8077d1f350
Deepak Kumar <dkk6477436@gmail.com>
2026-05-09 19:13:23 +0530

    Meeting summarizer, rag based chatbot code added

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -45,7 +45,7 @@ def convert_to_wav(input_path : str) -> str :
 # converted_audio = convert_to_wav(y)
 
 
-def chunk_audio(wav_path : str , chunk_minutes : int = 3 ) -> list:
+def chunk_audio(wav_path : str , chunk_minutes : int = 10 ) -> list:
 
     audio = AudioSegment.from_wav(wav_path)
     chunk_ms = chunk_minutes * 60 * 1000
